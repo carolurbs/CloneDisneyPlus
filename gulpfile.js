433d067ef1af561ad54302dcf7cc5ec97da9937d
Caroline Urbainski <caroline.urbainski@hotmail.com>
2024-06-24 19:38:45 -0300

    SHOWS

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ function images(){
     .pipe(gulp.dest("./dist/images"));
 }
 
-exports.default =gulp.parallel(styles, images) ;
+exports.default =gulp.parallel(styles,images) ;
 exports.watch =function(){
     gulp.watch('./src/styles/*scss', gulp.parallel(styles))
 }
