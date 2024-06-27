aa44ff3a3f5afc11afb374023f06d62e26bca416
Caroline Urbainski <caroline.urbainski@hotmail.com>
2024-06-27 17:34:59 -0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'))
 const imagemin=require('gulp-imagemin');
+const uglify=require('gulp-uglify')
+const obfuscate = require('gulp-obfuscate');
 
 function styles(){
     return gulp.src("./src/styles/*.scss")
@@ -12,8 +14,16 @@ function images(){
     .pipe(imagemin())
     .pipe(gulp.dest("./dist/images"));
 }
+function script()
+{
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(obfuscate())
+    .pipe(gulp.dest('./dist/js'))
+}
 
-exports.default =gulp.parallel(styles,images) ;
+exports.default =gulp.parallel(styles,images,script) ;
 exports.watch =function(){
     gulp.watch('./src/styles/*scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js',gulp.parallel(script))
 }
